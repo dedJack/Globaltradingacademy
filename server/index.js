dc3c2f9d6d466f050339f1ab97c0e006688e5c12
Aman Choudhary <142047102+dedJack@users.noreply.github.com>
2025-03-30 13:07:05 +0530

    changes in index.js

--- server/index.js
+++ server/index.js
@@ -1,30 +1,25 @@
 require("dotenv").config()
 const express = require("express")
 const app = express()
-const port = process.env.PORT || 5001;
+const port = process.env.PORT || 5000;
 var cors = require("cors");
 const connectDB = require("./db");
 const authRouter = require("./routes/auth");
 const contactRoute = require("./routes/contact");
-const adminRoute = require("./routes/admin");
 const errorMiddleware = require("./middlewares/error");
+const allowedOrigins = process.env.SERVER_API_URL 
 // const allowedOrigins = "http://localhost:3000"
-const allowedOrigins = [
-  "https://globaltradingacademy.in",
-  "https://www.globaltradingacademy.in", // Add the www version if needed
-  // "http://localhost:3000"
-];
+
 const corsOptions = {
   origin: function (origin, callback) {
-    // Allow requests with no origin, e.g., mobile apps or curl requests
-    if (!origin || allowedOrigins.includes(origin)) {
-      callback(null, true);
+    if (!origin || allowedOrigins.indexOf(origin) !== -1) {
+      callback(null, true); // Allow the origin
     } else {
       callback(new Error("Not allowed by CORS"));
     }
   },
   methods: "GET, POST, PUT, DELETE, PATCH, HEAD",
-  credentials: true,
+  credentials: true, 
 };
 
 app.use(cors(corsOptions));
@@ -32,7 +27,6 @@ app.use(express.json());
 
 app.use("/api/auth", authRouter);
 app.use("/api/form", contactRoute);
-app.use("/api/admin", adminRoute);
 
 app.use(errorMiddleware)
 
