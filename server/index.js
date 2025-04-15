84dd8cfebf8b0d76365e86ccca588ba13bc76a29
Aman Choudhary <142047102+dedJack@users.noreply.github.com>
2025-04-15 23:57:00 +0530

    cors issue solved

--- server/index.js
+++ server/index.js
@@ -7,20 +7,25 @@ const connectDB = require("./db");
 const authRouter = require("./routes/auth");
 const contactRoute = require("./routes/contact");
 const errorMiddleware = require("./middlewares/error");
-const allowedOrigins = process.env.SERVER_API_URL 
+// const allowedOrigins = process.env.
 // const allowedOrigins = "http://localhost:3000"
+const allowedOrigins = [
+  "http://localhost:3000",
+  "https://globaltradingacademy.in"
+];
 
 const corsOptions = {
   origin: function (origin, callback) {
-    if (!origin || allowedOrigins.indexOf(origin) !== -1) {
-      callback(null, true); // Allow the origin
+    if (!origin || allowedOrigins.includes(origin)) {
+      callback(null, true);
     } else {
       callback(new Error("Not allowed by CORS"));
     }
   },
   methods: "GET, POST, PUT, DELETE, PATCH, HEAD",
-  credentials: true, 
+  credentials: true,
 };
+
 
 app.use(cors(corsOptions));
 app.use(express.json());
